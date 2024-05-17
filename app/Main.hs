8241e8e1be74a02262e918b583fcec4e0b4a3ac7
Tristan Cacqueray <tdecacqu@redhat.com>
2024-05-16 20:40:23 -0400

    Integrate foreign-store for hot reload

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,9 @@
 -- NOTE: If this is file is edited, please also copy and paste it into
 -- README.md.
 
-{-# language OverloadedStrings #-}
+{-# language OverloadedStrings, LambdaCase, BlockArguments #-}
 
-module Main ( main ) where
+module Main where
 
 import MyLib
 
@@ -17,11 +17,48 @@ import SDL
 
 import Control.Monad.Managed
 import Control.Monad.IO.Class ()
-import Control.Monad (unless)
+import Control.Monad (unless, void)
 import Control.Exception (bracket, bracket_)
 
+import Data.IORef
+import Control.Concurrent
+import Foreign.Store qualified as FS
+
+-- Store the render function behind a IORef for hot reload
+renderFuncStore :: FS.Store (IORef (Window -> IO ()))
+renderFuncStore = FS.Store 0
+
+-- The ghcid entry point
+mainGHCID :: IO ()
+mainGHCID = FS.lookupStore renderFuncStoreID >>= \case
+  -- This is a fresh reload, initialize the store and fork the main thread
+  Nothing -> do
+    -- Create and store the render func IORef
+    renderRef <- newIORef renderFunc
+    FS.writeStore renderFuncStore renderRef
+
+    -- Fork a OSThread that persists ghci reload
+    void $ Control.Concurrent.forkOS do
+      runMain \win -> do
+        renderFun <- readIORef renderRef
+        renderFun win
+      -- If the app quit, cleanup the store to restart fresh
+      FS.deleteStore renderFuncStore
+
+  -- This is a hot reload, just update the stored render func IORef
+  Just store -> do
+    renderRef <- FS.readStore store
+    writeIORef renderRef renderFunc
+ where
+   -- For some reason, FS.lookupStore needs the newtyped Store value
+   FS.Store renderFuncStoreID = renderFuncStore
+
+-- The regular entry point
 main :: IO ()
-main = do
+main = runMain renderFunc
+
+runMain :: (Window -> IO ()) -> IO ()
+runMain theRenderFunc = do
   -- Initialize SDL
   initializeAll
 
@@ -42,10 +79,31 @@ main = do
     -- Initialize ImGui's OpenGL backend
     managed_ $ bracket_ openGL3Init openGL3Shutdown
 
-    liftIO $ mainLoop window
+    liftIO $ runLoop window
+  where
+  runLoop window = unlessQuit do
+    theRenderFunc window
+    runLoop window
 
-mainLoop :: Window -> IO ()
-mainLoop window = unlessQuit $ do
+  -- Process the event loop
+  unlessQuit action = do
+    shouldQuit <- gotQuitEvent
+    unless shouldQuit action
+
+  gotQuitEvent = do
+    ev <- pollEventWithImGui
+
+    case ev of
+      Nothing ->
+        return False
+      Just event ->
+        (isQuit event ||) <$> gotQuitEvent
+
+  isQuit event =
+    eventPayload event == QuitEvent
+
+renderFunc :: Window -> IO ()
+renderFunc window = do
   -- Tell ImGui we're starting a new frame
   openGL3NewFrame
   sdl2NewFrame
@@ -64,21 +122,3 @@ mainLoop window = unlessQuit $ do
   openGL3RenderDrawData =<< getDrawData
 
   glSwapWindow window
-  mainLoop window
-  where
-  -- Process the event loop
-  unlessQuit action = do
-    shouldQuit <- gotQuitEvent
-    unless shouldQuit action
-
-  gotQuitEvent = do
-    ev <- pollEventWithImGui
-
-    case ev of
-      Nothing ->
-        return False
-      Just event ->
-        (isQuit event ||) <$> gotQuitEvent
-
-  isQuit event =
-    eventPayload event == QuitEvent
